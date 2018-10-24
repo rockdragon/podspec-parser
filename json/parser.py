598a76d737e994dfb93b7fba833c89516288eb90
rockdragon <moyerock@gmail.com>
2018-10-24 20:09:35 +0800

    feat: inspect PodSpecChild Object
    Change-Id: I9b51c6909b553c179b155d5e55759b135ce8d6a6

--- json/parser.py
+++ json/parser.py
@@ -6,10 +6,17 @@ class PodSpec(object):
         setattr(self, key, value)
 
 
-class PureObject(object):
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
+class PodSpecChild(object):
+    def __init__(self):
+        self._keys = []
 
+    def keys(self):
+        return self._keys
+
+    def __setitem__(self, key, value):
+        if key not in self.__dict__:
+            self._keys.append(key)
+            setattr(self, key, value)
 
 def _typeof(o, repr):
     return type(o).__name__ == repr
@@ -17,27 +24,30 @@ def _typeof(o, repr):
 
 def iterate_object(obj):
     if _typeof(obj, 'dict'):
-        obj2 = PureObject()
+        obj2 = PodSpecChild()
         for k, v in obj.items():
             obj2[k] = iterate_object(v)
+        return obj2
     else:
         return obj
 
 
-def main():
+def get_pod_spec():
+    result = PodSpec()
     with open('podspec.json', 'r') as f:
         json_dict = json.load(f)
-        result = PodSpec()
         for k, v in json_dict.items():
             if _typeof(v, 'dict'):
                 result[k] = iterate_object(v)
             else:
                 result[k] = v
-
-            print(k, v, type(v))
-
-        print(dir(result))
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    podspec = get_pod_spec()
+    for k in podspec.__dict__.keys():
+        if _typeof(getattr(podspec, k), 'PodSpecChild'):
+            print(getattr(podspec, k).keys())
+
+
